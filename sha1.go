04137ef4102ffec1a24f20a790353291a8ea16ff
delaemon <takeshi.music.5@gmail.com>
2015-09-22 23:23:21 +0900

    Fix bug

--- sha1.go
+++ sha1.go
@@ -1,9 +1,13 @@
 package sha1
-import "fmt"
+import (
+	"fmt"
+	"encoding/binary"
+	"bytes"
+)
 
 func Sha1(message string) string {
 
-	//Initialize variables:
+	// Initialize variables:
 	var h0 uint32 = 0x67452301
 	var h1 uint32 = 0xEFCDAB89
 	var h2 uint32 = 0x98BADCFE
@@ -11,27 +15,46 @@ func Sha1(message string) string {
 	var h4 uint32 = 0xC3D2E1F0
 
 	// Pre-processing:
-	m := []byte(message)
-	m = append(m, byte(0x80))
-	m = append(m, byte(0x00 * ((56 - (len(message) * 8 + 1) % 64) % 64)))
+	chunks := []byte(message)
 
+	// append the bit '1' to the message
+	chunks = append(chunks, byte(0x80))
+
+	// append 0 <= k < 512 bits '0', so that the resulting message length (in bits)
+	//    is congruent to 448 (mod 512)
+	padding := 64 - (len(message) + 9) % 64
+	for i := 0; i < padding; i++ {
+		chunks = append(chunks, 0x00)
+	}
+
+	// append length of message (before pre-processing), in bits, as 64-bit big-endian integer
+	ml := uint64ToBytes(uint64(len(message) * 8))
+	chunks = append(chunks[:len(chunks)], append([]byte{}, ml...)...)
+
+	// Process the message in successive 512-bit chunks:
+	// break message into 512-bit chunks
 	words := make([]uint32, 80, 80)
+
+	// break chunk into sixteen 32-bit big-endian words w[i]
 	for i := 0; i < 16; i++ {
-		chunk := message[0:4]
-		words[i] = uint32(chunk[3] | (chunk[2] << 8) | (chunk[1] << 16) | (chunk[0] << 24))
+		chunk := chunks[i*4:i*4+4]
+		words[i] = binary.BigEndian.Uint32(chunk)
 	}
 
+	// Extend the sixteen 32-bit words into eighty 32-bit words:
 	for i := 16; i < 80; i++ {
-		n := words[i - 3] ^ words[i - 8] ^ words[i - 14] ^ words[i - 16];
-		words[i] = LeftRotate(n, 1)
+		n := words[i - 3] ^ words[i - 8] ^ words[i - 14] ^ words[i - 16]
+		words[i] = leftRotate(n, 1)
 	}
 
+	// Initialize hash value for this chunk:
 	a := h0
 	b := h1
 	c := h2
 	d := h3
 	e := h4
 
+	// Main loop:
 	var f, k uint32
 	for i := 0; i < 80; i++ {
 		if 0 <= i && i <= 19 {
@@ -48,18 +71,27 @@ func Sha1(message string) string {
 			k = 0xCA62C1D6
 		}
 
-		a, b, c, d, e = LeftRotate(a, uint32(5)) + f + e + k + words[i] & 0xffffffff, a, LeftRotate(b, uint32(30)), c, d
+		a, b, c, d, e = leftRotate(a, uint32(5)) + f + e + k + words[i] & 0xffffffff, a, leftRotate(b, uint32(30)), c, d
 	}
 
+	// Add this chunk's hash to result so far:
 	h0 = (h0 + a) & 0xffffffff
 	h1 = (h1 + b) & 0xffffffff
 	h2 = (h2 + c) & 0xffffffff
 	h3 = (h3 + d) & 0xffffffff
 	h4 = (h4 + e) & 0xffffffff
 
+	// Produce the final hash value (big-endian)
 	return fmt.Sprintf("%08x%08x%08x%08x%08x", h0, h1, h2, h3, h4)
 }
 
-func LeftRotate(n, b uint32) uint32 {
+func leftRotate(n, b uint32) uint32 {
 	return ((n << b) | (n >> (32 - b))) & 0xffffffff
 }
+
+func uint64ToBytes(data uint64) []byte {
+	buf := new(bytes.Buffer)
+	binary.Write(buf, binary.BigEndian, data)
+	return buf.Bytes()
+}
+
--- sha1_test.go
+++ sha1_test.go
@@ -1,11 +1,28 @@
 package sha1
 
-import (
-	"testing"
-)
+import "testing"
+
+type Test struct {
+	in string
+	out string
+}
+
+var tests = []Test{
+	{"The quick brown fox jumps over the lazy dog",
+	"2fd4e1c67a2d28fced849ee1bb76e7391b93eb12"},
+	{"The quick brown fox jumps over the lazy cog",
+	"de9f2c7fd25e1b3afad3e85a0bd17d9b100db4b3"},
+	{"", "da39a3ee5e6b4b0d3255bfef95601890afd80709"},
+	{"testing\n", "9801739daae44ec5293d4e1f53d3f4d2d426d91c"},
+	{"xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+	"025ecbd5d70f8fb3c5457cd96bab13fda305dc59"},
+}
 
 func TestSha1(t *testing.T) {
-	var r string
-	r = Sha1("still unfinished")
-	t.Logf("%s",r)
+	for i, test := range tests {
+		out := Sha1(test.in)
+		if out != test.out {
+			t.Errorf("#%d: Sha1(%s) = %s; want %s", i, test.in, out, test.out)
+		}
+	}
 }
